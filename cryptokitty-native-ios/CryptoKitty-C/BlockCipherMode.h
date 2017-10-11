5c2ceddb1b72aea8276a65b5a33424f0856cfcc3
Steve Brenneis <steve.brenneis@secomm.org>
2017-10-11 15:03:22 -0400

    Initial commit.

---
+++ cryptokitty-native-ios/CryptoKitty-C/BlockCipherMode.h
@@ -0,0 +1,27 @@
+#ifndef BLOCKCIPHERMODE_H_INCLUDED
+#define BLOCKCIPHERMODE_H_INCLUDED
+
+#import <coder_iOS/coder_iOS.h>
+
+class BlockCipherMode {
+
+    protected:
+        BlockCipherMode() {}
+        
+    public:
+        virtual ~BlockCipherMode() {}
+
+    private:
+        BlockCipherMode(const BlockCipherMode& other);
+        BlockCipherMode& operator= (const BlockCipherMode& other);
+
+    public:
+        virtual coder::ByteArray decrypt(const coder::ByteArray& ciphertext,
+                                            const coder::ByteArray& key)=0;
+        virtual coder::ByteArray encrypt(const coder::ByteArray& plaintext,
+                                            const coder::ByteArray& key)=0;
+        virtual void setIV(const coder::ByteArray& iv)=0;
+
+};
+
+#endif  // BLOCKCIPHERMODE_H_INCLUDED
