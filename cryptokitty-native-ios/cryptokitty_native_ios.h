b62492bdd7cae519bbf4e3144a2ad3ea50f6abe0
Steve Brenneis <steve.brenneis@secomm.org>
2017-10-11 14:51:02 -0400

    Initial commit.

---
+++ cryptokitty-native-ios/cryptokitty_native_ios.h
@@ -0,0 +1,28 @@
+//
+//  cryptokitty_native_ios.h
+//  cryptokitty-native-ios
+//
+//  Created by Steve Brenneis on 10/11/17.
+//  Copyright © 2017 seComm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cryptokitty_native_ios.
+FOUNDATION_EXPORT double cryptokitty_native_iosVersionNumber;
+
+//! Project version string for cryptokitty_native_ios.
+FOUNDATION_EXPORT const unsigned char cryptokitty_native_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cryptokitty_native_ios/PublicHeader.h>
+
+#import <CryptoKitty_iOS/CKRSAPrivateKey.h>
+#import <CryptoKitty_iOS/CKRSAPublicKey.h>
+#import <CryptoKitty_iOS/CKRSAKeyPair.h>
+#import <CryptoKitty_iOS/CKRSAKeyPairGenerator.h>
+#import <CryptoKitty_iOS/CKSecureRandom.h>
+#import <CryptoKitty_iOS/CKSHA1.h>
+#import <CryptoKitty_iOS/CKSHA256.h>
+#import <CryptoKitty_iOS/CKPEMCodec.h>
+#import <CryptoKitty_iOS/CKRSACodec.h>
+
